71e93af5983d68acefd3600d2ab544eeeb0831d5
nikitashpota <nikitashpota@ya.ru>
2021-04-26 09:39:39 +0300

    #7

--- js/script.js
+++
@@ -1,7 +0,0 @@
-let button = document.getElementById('button');
-
-let navigation = document.getElementById('navigation');
-
-button.addEventListener('click', function () {
-    navigation.classList.toggle('display-block');
-});
